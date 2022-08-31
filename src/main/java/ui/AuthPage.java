90031ffe1af65f2cae5a7e069a28cbeaac8157c3
Daria Nos <ddnos@alfabank.ru>
2022-08-31 17:00:19 +0300

    refactoring

--- src/main/java/ui/AuthPage.java
+++ src/main/java/ui/AuthPage.java
@@ -1,11 +1,53 @@
 package ui;
 
-import io.appium.java_client.AppiumDriver;
+import java.util.List;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import lombok.Getter;
 
+@Getter
 public class AuthPage extends CorePageObject {
 
-    public AuthPage(AppiumDriver driver) {
-        super(driver);
+    @AndroidFindBy(id = "login_button")
+    private MobileElement loginButton;
+
+    @AndroidFindBy(id = "logo")
+    private MobileElement logo;
+
+    @AndroidFindBy(id = "email_or_phone")
+    private MobileElement emailOrPhone;
+
+    @AndroidFindBy(id = "vk_password")
+    private MobileElement vkPassword;
+
+    @AndroidFindBy(id = "continue_btn")
+    private MobileElement continueButton;
+
+    @AndroidFindBy(id = "login_error_title")
+    private MobileElement loginErrorTitle;
+
+    @AndroidFindBy(id = "login_error_subtitle")
+    private MobileElement loginErrorSubtitle;
+
+    public AuthPage() {
+        super();
+    }
+
+    public void initSingIn() {
+        loginButton.click();
+    }
+
+    public void confirmPageLoad() {
+        confirmPageLoad(List.of(logo));
+    }
+
+    public void fillLogoPass(String login, String password) {
+        emailOrPhone.sendKeys(login);
+        vkPassword.sendKeys(password);
+    }
+
+    public void submitLogin() {
+        continueButton.click();
     }
 
 }
